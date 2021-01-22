8607b41000f8ef66fc585794e37e5c00a62cdc82
vishwasranjan <vishwasranjan16@gmail.com>
2021-01-23 02:59:53 +0530

    Navigation bar

--- app/src/main/java/com/example/prabishastartupnetwork/WelcomeActivity.java
+++ app/src/main/java/com/example/prabishastartupnetwork/WelcomeActivity.java
@@ -1,21 +1,68 @@
 package com.example.prabishastartupnetwork;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class WelcomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class WelcomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
+    private DrawerLayout drawer;
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle Toggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(Toggle);
+        Toggle.syncState();
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.nav_home:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragentt()).commit();
+                break;
+            case R.id.nav_blog:
+
+                //
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
